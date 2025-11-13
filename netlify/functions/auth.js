93234b144120907f6ab48fc8c26c17b8413b3e66
Scott Shapiro <scott@scottshapiro.com>
2025-11-13 14:03:27 -0700

    Fix auth function to handle OAuth callback properly

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,13 +1,22 @@
 exports.handler = async (event, context) => {
-  const { queryStringParameters } = event;
-  const params = new URLSearchParams(queryStringParameters || {});
+  const { queryStringParameters, path } = event;
   
-  // For PKCE flow, just redirect to GitHub OAuth with the parameters
-  // Netlify CMS will handle the PKCE flow client-side
+  // If this is a callback (has 'code' parameter), let it pass through to /admin/
+  if (queryStringParameters && queryStringParameters.code) {
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `https://scottshapiro.com/admin/?${new URLSearchParams(queryStringParameters).toString()}`
+      }
+    };
+  }
+  
+  // Otherwise, redirect to GitHub OAuth
+  const params = new URLSearchParams(queryStringParameters || {});
   const clientId = 'Ov23liO3UDaEIBmLhm4p';
   const redirectUri = 'https://scottshapiro.com/admin/';
   
-  // Build GitHub OAuth URL
+  // Build GitHub OAuth URL with PKCE parameters
   const githubParams = new URLSearchParams({
     client_id: clientId,
     redirect_uri: redirectUri,
@@ -15,9 +24,13 @@ exports.handler = async (event, context) => {
     state: params.get('state') || Math.random().toString(36).substring(7)
   });
   
-  // If there's a code_verifier (PKCE), add it
+  // Add PKCE parameters if present
   if (params.get('code_verifier')) {
     githubParams.append('code_verifier', params.get('code_verifier'));
+  }
+  if (params.get('code_challenge')) {
+    githubParams.append('code_challenge', params.get('code_challenge'));
+    githubParams.append('code_challenge_method', 'S256');
   }
   
   const githubAuthUrl = `https://github.com/login/oauth/authorize?${githubParams.toString()}`;
